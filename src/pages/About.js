e26bb438d582a573e08b97dbb3c09a52f53db8c8
franckkoukougnon <franckkoukougnon@gmail.com>
2024-11-25 03:25:46 +0100

    my training in react

---
+++ src/pages/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        quibusdam, voluptatibus, quos, quod natus dolorum quia quae repellendus
+        quae, aspernatur quia. Quisquam quibusdam, voluptatibus, quos, quod
+        natus dolorum quia quae repellend
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe corrupti
+        illum unde earum non suscipit, dolorem enim aliquid quidem. Nisi, earum.
+        Magnam aperiam nostrum omnis tempore mollitia, culpa aliquid ducimus vel
+        delectus? Deleniti quis asperiores consequatur facilis sint blanditiis
+        quod. Est, sunt ex sit unde et necessitatibus illo soluta dolor ullam
+        officia error, explicabo perspiciatis repellat repellendus quasi
+        praesentium veniam delectus reiciendis rem architecto nobis tempora
+        voluptatum.
+      </p>
+    </div>
+  );
+};
+
+export default About;
